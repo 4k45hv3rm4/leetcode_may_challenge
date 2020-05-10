a5c6583de806ce6b89de5de90102971c942086b9
4k45hv3rm4 <4k45hr0ck5007@gmail.com>
2020-05-10 21:13:13 +0530

    may-10

---
+++ may-10.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        if len(trust)< N-1:
+            return -1
+
+        trust_scores =[0]*(N+1)
+        for out_,in_ in trust:
+            trust_scores[out_]-=1
+            trust_scores[in_]+=1
+
+        for k , score in enumerate(trust_scores[1:], 1):
+            if score==N-1:
+                return k
+        return -1
---
+++ may_9.py
@@ -0,0 +1,12 @@
+x =2147483647
+import math
+# x1 = math.sqrt()
+
+# print(x1)
+c = list(range(1, x//2))
+if x1 in c:
+    print("True")
+
+# for i in range(1, x+1):
+#     if (math.sqrt(x)==i):
+#         print(i)
